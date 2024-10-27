d38944f9b3fe27c42a94943b80c0db5bae96456b
Luiz Carlos <121176269+LcsfCarlos@users.noreply.github.com>
2024-10-27 09:22:09 +0000

    Adcionado scss

--- src/app/modules/portfolio/components/experiences/experiences.component.ts
+++ src/app/modules/portfolio/components/experiences/experiences.component.ts
@@ -14,45 +14,45 @@ export class ExperiencesComponent {
   public arrayExperiences = signal<IExperiences[]>([
     {
       summary: {
-        strong: "Front-end Especialista",
-        p:"ZUP | Set 2020 - Present",
+        strong: "Angular",
+        p:"ZUP | Set 2022 - Present",
       },
-      text: "<p>Desde setembro de 2020, atuo como Analista de Sistema Especialista na ZUP, destacando-me nas áreas Front-end, utilizando Html5, Css3, Javascript, Typescript, Angular 2+, Angular Material e NgRx, Back-end, onde o NodeJs é aplicado em Provas de conceito, caso necessário.</p>",
+      text: "<p>Em maio de 2023 realizei meu primeiro curso com a utilização do Angular framework, iniciando assim o conhecimento de diversas skills com o angular, formulários reativos, rotas, onde desenvolvi alguns projetos práticos como To-do-List, Pokedex, Dark-Theme, Auth-Front, Meu-Portfólio, onde sigo estudando e buscando conhecer as novidades da sua interface e tentando sempre me manter atualizado.</p>",
     },
     {
       summary: {
-        strong: "Fullstack Developer",
-        p:"ZUP | Set 2020 - Present",
+        strong: "GitHub",
+        p:"ZUP | Ago 2024 - Present",
       },
-      text: "<p>Desde setembro de 2020, atuo como Analista de Sistema Especialista na ZUP, destacando-me nas áreas Front-end, utilizando Html5, Css3, Javascript, Typescript, Angular 2+, Angular Material e NgRx, Back-end, onde o NodeJs é aplicado em Provas de conceito, caso necessário.</p>",
+      text: "<p>Realizei um curso de Git e GitHub, onde aprendi sobre armazenar e administrar o código, fazer o registro de todas as mudanças conforme o avanço de cada projeto, compartilhamento de projetos, e colaboração em projetos de outra pessoa, assim como seus comandos básicos, criação de novas branchs, pull requests e incorporação de mudanças.</p>",
     },
     {
       summary: {
-        strong: "Consultor Web",
-        p:"ZUP | Set 2020 - Present",
+        strong: "TypeScript",
+        p:"ZUP | Abril 2023 - Set 2024",
       },
-      text: "<p>Desde setembro de 2020, atuo como Analista de Sistema Especialista na ZUP, destacando-me nas áreas Front-end, utilizando Html5, Css3, Javascript, Typescript, Angular 2+, Angular Material e NgRx, Back-end, onde o NodeJs é aplicado em Provas de conceito, caso necessário.</p>",
+      text: "<p>Em abril de 2023 realizei um curso sobre Typscript na plataforma da Udemy conhecendo desde os conceitos básicos de criação da linguagem, tipagem de dados em variáveis, além dos recursos avançados de orientação a objeto e padrões em projetos, seus tipos, classes, interface e compilador. Em 2024 refiz uma revisão a respeito dos conhecimentos.</p>",
     },
     {
       summary: {
-        strong: "Fullstack Developer",
-        p:"ZUP | Set 2020 - Present",
+        strong: "JavaScript",
+        p:"ZUP | Fev 2023 - Julh 2024",
       },
-      text: "<p>Desde setembro de 2020, atuo como Analista de Sistema Especialista na ZUP, destacando-me nas áreas Front-end, utilizando Html5, Css3, Javascript, Typescript, Angular 2+, Angular Material e NgRx, Back-end, onde o NodeJs é aplicado em Provas de conceito, caso necessário.</p>",
+      text: "<p>Realizei um curso pela plataforma da Udemy onde iniciei o conhecimento sobre JAVASCRIPT, tipos primitivos, operadores aritméticos, variáveis, condições, funções, objetos rasos e profundos, array a fundo. Realizei também um curso de JavaScipt patrocinado pelo Google em julho de 2024 com diversas aulas desde fundamentos e realização de exercícios.</p>",
     },
     {
       summary: {
-        strong: "Decolar.com Front-end",
-        p:"ZUP | Set 2020 - Present",
+        strong: "Manipulando DOM",
+        p:"ZUP | Abril 2023",
       },
-      text: "<p>Desde setembro de 2020, atuo como Analista de Sistema Especialista na ZUP, destacando-me nas áreas Front-end, utilizando Html5, Css3, Javascript, Typescript, Angular 2+, Angular Material e NgRx, Back-end, onde o NodeJs é aplicado em Provas de conceito, caso necessário.</p>",
+      text: "<p>Aprendi sobre o conceito do DOM e seus elementos, diversas maneiras de selecionar elementos, manipular valores, manipular estilos, encontrar elementos (First Child e First Element Child), seus eventos em geral, adicionar e remover valores, delegar eventos, validar eventos, e por fim diversas maneiras de trabalhar com formulários, entrada e saída de dados nos formulários, manipulação de input e checkbox.</p>",
     },
     {
       summary: {
-        strong: "EW21 Front-end",
-        p:"ZUP | Set 2020 - Present",
+        strong: "Inicio na Programação Jornada Front-End HTML, CSS3, SASS, BOOTSTRAP",
+        p:"ZUP | Dez 2022 - Fev 2023 ",
       },
-      text: "<p>Desde setembro de 2020, atuo como Analista de Sistema Especialista na ZUP, destacando-me nas áreas Front-end, utilizando Html5, Css3, Javascript, Typescript, Angular 2+, Angular Material e NgRx, Back-end, onde o NodeJs é aplicado em Provas de conceito, caso necessário.</p>",
+      text: "<p>Iniciei na programação em dezembro de 2022, começando com o básico no Front-End, onde aprendi a base em HTML, após esse início realizei cursos e projetos práticos com CSS3, SASS, E BOOTSTRAP, aprendendo muitas maneiras de manipular toda estrutura de classes, cores e detalhes da página web.</p>",
     },
   ]);
 }
--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,30 +1,104 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 // 
 import { IProjects } from '../../interface/IProjects.interface';
 
+// Material
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+
+// Enum
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+
+// Dialog
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
+
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+    #dialog = inject(MatDialog);
+
     public arrayProjects = signal<IProjects[]>([
       {
         src: "assets/img/projects/vfull.png",
-        alt: "Projeto Vida FullStack",
-        title: "Vida FullStack",
+        alt: "Projeto Meu Primeiro Site",
+        title: "Meu Primeiro Site",
         width: "100px",
         height: "51px",
-        description: "<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular</p>",
+        description: "<p>Esse foi meu primeiro projeto desenvolvido, simples, utilizando os conhecimentos estudados HTML, CSS3, SASS e FLEXBOX.</p>",
         links: [
           {
-              name: "Conheça o Blog",
-              href: "https://vidafullstack.com.br"
+              name: "GH-Pages",
+              href: "https://lcsfcarlos.github.io/hands-on-curso-front-end-1"
+          },
+        ],
+      },
+      {
+        src: "assets/img/projects/vfull.png",
+        alt: "Site Loja Virtual",
+        title: "Site Loja Virtual",
+        width: "100px",
+        height: "51px",
+        description: "<p>Loja Virtual de roupas, desenvolvida utilizando os conhecimentos estudados, HTML, CSS3 E SASS e FLEXBOX. </p>",
+        links: [
+          {
+              name: "GH-Pages",
+              href: "https://lcsfcarlos.github.io/hands-on-curso-front-end-2/"
+          },
+        ],
+      },
+      {
+        src: "assets/img/projects/vfull.png",
+        alt: "Lista de Tarefas",
+        title: "Lista de Tarefas",
+        width: "100px",
+        height: "51px",
+        description: "<p>Lista de taferas desenvolvida onde é possivel adcionar novas tarefas, excluir, e salvar novas tarefas, projeto simples utilizando os conhecimentos estudados, TYPESCRIPT, CSS, SASS, HTML, FLEXBOX.</p>",
+        links: [
+          {
+              name: "GH-Pages",
+              href: "https://lcsfcarlos.github.io/Curso-Angular-To-do-List/"
+          },
+        ],
+      },
+      {
+        src: "assets/img/projects/vfull.png",
+        alt: "Dark-Theme",
+        title: "Dark-Theme",
+        width: "100px",
+        height: "51px",
+        description: "<p>Projeto desenvolvido onde o tema muda de acordo com o horário e tambem o tema e a imagem, utilizando os conhecimentos estudados TYPESCRIPT, CSS, SASS, FLEXBOX</p>",
+        links: [
+          {
+              name: "GH-Pages",
+              href: "https://lcsfcarlos.github.io/Curso-angular-dark-theme/"
+          },
+        ],
+      },
+      {
+        src: "assets/img/projects/vfull.png",
+        alt: "Projeto Meu Portfólio",
+        title: "Projeto Meu Portfólio",
+        width: "100px",
+        height: "51px",
+        description: "<p>Projeto meu portfólio, desenvolvido com os conhecimentos estudados utilizando o ANGULAR, HTML, CSS, SASS, TYPESCRIPT E FLEXBOX, onde apresento dados pessoais para contato e tambem um pouco do meu estudo e trabalhos desenvolvidos, onde sigo na jornada de aprendizado buscando me atualizar e sempre aprender coisas novas.</p>",
+        links: [
+          {
+              name: "GitHub",
+              href: "https://github.com/LcsfCarlos"
           },
         ],
       },
     ]);
+
+    public openDialog(data: IProjects){
+      this.#dialog.open(DialogProjectsComponent, {
+        data,
+        panelClass: EDialogPanelClass.PROJECTS
+      })
+    }
   }
